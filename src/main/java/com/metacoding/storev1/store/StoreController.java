e81d7ea603417c06f321a55162e1bb7434b34988
roni <ksri2457@gmail.com>
2025-03-20 12:18:53 +0900

    store save

--- src/main/java/com/metacoding/storev1/store/StoreController.java
+++ src/main/java/com/metacoding/storev1/store/StoreController.java
@@ -4,13 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller // IoC(제어의 역전) => HashSet
+@Controller // IoC(제어의 역전전) => HashSet
 public class StoreController {
+
+    private StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
 
     @GetMapping("/")
     public String list() {
         return "store/list";
+    }
+
+    @GetMapping("/store/{id}")
+    public String detail(@PathVariable("id") int id) {
+        return "store/detail";
+    }
+
+    @GetMapping("/store/{id}/update-form")
+    public String updateForm(@PathVariable("id") int id) {
+        return "store/update-form";
+    }
+
+    @PostMapping("/store/{id}/delete")
+    public String delete(@PathVariable("id") int id) {
+        return "redirect:/";
     }
 
     @GetMapping("/store/save-form")
@@ -18,23 +40,17 @@ public class StoreController {
         return "store/save-form";
     }
 
-    @GetMapping("/store/{id}")
-    public String detail(@PathVariable("id") Integer id) {
-        return "store/detail";
-    }
-
-    @GetMapping("/store/{id}/update-form")
-    public String updateForm(@PathVariable("id") Integer id) {
-        return "store/update-form";
-    }
-
-    @GetMapping("/store/{id}/delete")
-    public String delete(@PathVariable("id") Integer id) {
-        return "redirect:/";
-    }
-
     @PostMapping("/store/save")
-    public String save() {
+    public String save(@RequestParam("name") String name, @RequestParam("stock") int stock,
+            @RequestParam("price") int price) {
+
+        storeService.상품등록(name, stock, price);
+
         return "redirect:/";
+    }
+
+    @PostMapping("/store/{id}/update")
+    public String update(@PathVariable("id") int id) {
+        return "redirect:/store/" + id;
     }
 }
--- src/main/java/com/metacoding/storev1/store/StoreRepository.java
+++ src/main/java/com/metacoding/storev1/store/StoreRepository.java
@@ -1,5 +1,27 @@
 package com.metacoding.storev1.store;
 
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+// import lombok.RequiredArgsConstructor;
+import jakarta.persistence.Query;
+
+// @RequiredArgsConstructor // final 붙은 필드로만 생성자 만들어
+@Repository
 public class StoreRepository {
+
+    private EntityManager em;
+
+    public StoreRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(String name, int stock, int price) {
+        Query query = em.createNativeQuery("insert into store_tb(name, stock, price) values('바나나', 40, 3000)");
+        query.setParameter(1, name);
+        query.setParameter(2, stock);
+        query.setParameter(3, price);
+        query.executeUpdate();
+    }
 
 }
